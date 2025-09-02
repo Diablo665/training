10e1ccf3c15c02663ca482b4ac7c943ef3a5e65a
Olive <152081711+Diablo665@users.noreply.github.com>
2025-09-03 02:41:48 +0300

    Функции разбиты на отдельные блоки

--- frontend/app-todo-list/js/main.js
+++ frontend/app-todo-list/js/main.js
@@ -1,17 +1,19 @@
-import {keyListener, menuButtonListener, autoResize, mainKeyHandler} from './events/eventHandlers.js';
+import {keyListener, menuButtonListener, autoResize, mainKeyHandler, selectListener, searchListener} from './events/eventHandlers.js';
 import {loader} from './utils/helper.js'
 import {textarea, renderTask} from './tasks/taskModel.js';
 import { setThemeInLoad } from './utils/themes.js';
+import { search } from './filters/filters.js';
 
 document.addEventListener('DOMContentLoaded', function(){
     setThemeInLoad();
     loader(1);
 
-    menuButtonListener();
-    keyListener()
     textarea.addEventListener('input', autoResize);
 
+    menuButtonListener();
     renderTask()
-
+    keyListener()
     mainKeyHandler()
+    selectListener()
+    searchListener()
 });
